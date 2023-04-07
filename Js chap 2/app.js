afe4dd8451786ef97de6d26161d354f6c3bf0f57
Maham <mahamiqbal931@gmail.com>
2023-04-07 19:51:58 +0500

    second commit

--- Js chap 2/app.js
+++ Js chap 2/app.js
@@ -1,33 +1,51 @@
-//===============CHAPTER 1 ALERTS================//
-//Q1. 
- //alert ("Welcome here...");
-
+//====================CHAPTER 2 VARIABLES=======================//
+//Question1. 
+    //var username = "Maham Iqbal";
 
 
 //Question2. 
- //alert ("Error! Please enter a valid password.");
-
+    //var myName = "Maham Iqbal";
 
 
 //Question3. 
- //alert ("Welcome to JS Land... \n Happy Coding!");
+    //var message = "Hello World"
+    //alert (message);
 
 
+//Question4.
+    //var studentName = "Jhone Doe";
+    //alert (studentName);
 
-//Question4. 
- //alert("Welcome to JS Land...");
- //alert("Happy Coding!");
+    //var studentAge = "15 years old";
+    //alert (studentAge);
+
+    //var studentSkill = "Certified Mobile Application Development";
+    //alert (studentSkill);
 
 
-//Question5. 
- //alert("Hello... I can run JS through my web browser's console.");
+//Question5.
+    //var name = " \n PIZZA \n PIZZ \n PIZ \n PI \n P";
+    //alert (name);
 
 
+//Question6. 
+    //var email = "example@example.com";
+    //alert ( "My email address is " + email);
+        
 
-//Question6.
- //alert("Hello!");
- //alert("Glad to see you here.");
- //alert("Hope that it will be useful for you".);
+//Question7. 
+    //var book = "A smarter way to learn JavaScript";
+    //alert ("I am trying to learn from the book " + book);
 
-  // ===== assignment 2 completed=====//
+
+//Question8.
+    //document.write("Yah I can write HTML content through JavaScript")
+
+
+//Question9.
+var string = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬";
+alert (string);
+
+
+//========assignment 2 completed===========//
 
