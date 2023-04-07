b54df42a7ea79b77ce196a3f71b5cc1a96bb8f37
Maham <mahamiqbal931@gmail.com>
2023-04-07 18:25:50 +0500

    first commit

---
+++ Js chap 2/app.js
@@ -0,0 +1,33 @@
+//===============CHAPTER 1 ALERTS================//
+//Q1. 
+ //alert ("Welcome here...");
+
+
+
+//Question2. 
+ //alert ("Error! Please enter a valid password.");
+
+
+
+//Question3. 
+ //alert ("Welcome to JS Land... \n Happy Coding!");
+
+
+
+//Question4. 
+ //alert("Welcome to JS Land...");
+ //alert("Happy Coding!");
+
+
+//Question5. 
+ //alert("Hello... I can run JS through my web browser's console.");
+
+
+
+//Question6.
+ //alert("Hello!");
+ //alert("Glad to see you here.");
+ //alert("Hope that it will be useful for you".);
+
+  // ===== assignment 2 completed=====//
+
